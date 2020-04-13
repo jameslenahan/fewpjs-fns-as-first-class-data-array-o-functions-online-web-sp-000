3822e49aac8eef3d283ee2da080397cc69384e3f
James Lenahan <james.k.lenahan@gmail.com>
2020-04-13 20:43:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(dog, breed){
+    let completedRoutine = [];
+    routine.forEach( function(func){
+        completedRoutine.push(func(dog, breed));
+    });
+    return completedRoutine;
+}
