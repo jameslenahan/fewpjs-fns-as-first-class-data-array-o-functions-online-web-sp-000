145331054be84c15cdbacaf06e4af7bf6cf35553
James Lenahan <james.k.lenahan@gmail.com>
2020-04-13 20:45:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ function leashDog(dog, breed){
 function walkToPark(dog, breed){
     return `Walk to the park with ${dog} the ${breed}`;
 }
+function throwFrisbee(dog, breed){
+    return `Throw the frisbee for ${dog} the ${breed}`;
+}
+
+function walkHome(dog, breed){
+    return `Walk home with ${dog} the ${breed}`;
+}
+
+function unleashDog(dog, breed){
+    return `Unleash ${dog} the ${breed}`;
+}
